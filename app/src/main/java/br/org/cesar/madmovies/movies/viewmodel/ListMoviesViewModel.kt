2a8363b5d71672fa45b324816ed5998c6f0c3022
Eduardo Maia <eduardoalmaia@gmail.com>
2021-10-25 12:54:31 -0300

    Consult API after subscription
    After this change, the use case will be called
    after the first subscription, and the value will
    be cached and emitted for every other subscription.
    
    The cached value will be erased if there are no
    subscriptions after 2 seconds, so the cache is not erased
    on configuration changes.
    If a subscription happens after the cache is erased a
    new call will be made to the use case
    
    Fixes #1
    
    After this change, the use case will be called
    after the first subscription, and the value will be cached
    and emitted for every other subscription.
    
    The cached value will be erased if there are no subscriptions. If a
    subscription happens after the cache is erased a new call will be made
    to the use case

--- app/src/main/java/br/org/cesar/madmovies/movies/viewmodel/ListMoviesViewModel.kt
+++ app/src/main/java/br/org/cesar/madmovies/movies/viewmodel/ListMoviesViewModel.kt
@@ -5,8 +5,7 @@ import androidx.lifecycle.viewModelScope
 import br.org.cesar.madmovies.movies.data.remote.RemoteRepository
 import br.org.cesar.madmovies.movies.domain.model.Movie
 import br.org.cesar.madmovies.movies.domain.usecase.GetMovieList
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 class ListMoviesViewModel : ViewModel() {
@@ -14,10 +13,8 @@ class ListMoviesViewModel : ViewModel() {
     private var _movieListFlow: MutableStateFlow<List<Movie>> = MutableStateFlow(mutableListOf())
     var movieListFlow: StateFlow<List<Movie>> =
         _movieListFlow
-
-    init {
-        updateMovieList()
-    }
+            .onSubscription { updateMovieList() }
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(2000), _movieListFlow.value)
 
     fun updateMovieList() {
         viewModelScope.launch {
