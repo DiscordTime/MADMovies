e8f86a3f054453a706889d3612e02455d2accf84
ehammo <eduardoalmaia@gmail.com>
2022-01-31 09:11:56 -0300

    Fixed data classes identations

--- app/src/main/java/br/org/cesar/madmovies/movies/data/model/RemoteMovie.kt
+++ app/src/main/java/br/org/cesar/madmovies/movies/data/model/RemoteMovie.kt
@@ -5,19 +5,19 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class RemoteMovie(
- val adult: Boolean = false,
- val image: String = "",
- val genre_ids: List<Int> = listOf(),
- val id: Int = 0,
- val original_language: String = "",
- val original_title: String = "",
- val overview: String = "",
- val popularity: Double = 0.0,
- val poster_path: String? = "",
- val release_date: String? = "",
- val title: String = "",
- val vote_average: Double = 0.0,
- val vote_count: Int = 0
+    val adult: Boolean = false,
+    val image: String = "",
+    val genre_ids: List<Int> = listOf(),
+    val id: Int = 0,
+    val original_language: String = "",
+    val original_title: String = "",
+    val overview: String = "",
+    val popularity: Double = 0.0,
+    val poster_path: String? = "",
+    val release_date: String? = "",
+    val title: String = "",
+    val vote_average: Double = 0.0,
+    val vote_count: Int = 0
 )
 
 fun RemoteMovie.asMovie() = Movie(id, title, release_date ?: "", overview, popularity)
--- app/src/main/java/br/org/cesar/madmovies/movies/domain/model/Movie.kt
+++ app/src/main/java/br/org/cesar/madmovies/movies/domain/model/Movie.kt
@@ -1,10 +1,11 @@
 package br.org.cesar.madmovies.movies.domain.model
 
-data class Movie(val id: Int,
-                 val title: String,
-                 val year: String,
-                 val overview: String,
-                 val popularity: Double
-                 ) {
-    constructor(): this(-1, "", "", "", 0.0)
+data class Movie(
+    val id: Int,
+    val title: String,
+    val year: String,
+    val overview: String,
+    val popularity: Double
+) {
+    constructor() : this(-1, "", "", "", 0.0)
 }
