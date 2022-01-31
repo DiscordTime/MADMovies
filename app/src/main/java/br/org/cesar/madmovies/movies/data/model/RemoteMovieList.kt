89df56ca1f330d425a9dfcac46073c484513e36d
ehammo <eduardoalmaia@gmail.com>
2022-01-31 09:11:56 -0300

    Fixed data classes identations

--- app/src/main/java/br/org/cesar/madmovies/movies/data/model/RemoteMovieList.kt
+++ app/src/main/java/br/org/cesar/madmovies/movies/data/model/RemoteMovieList.kt
@@ -4,7 +4,8 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class RemoteMovieList(
-    val page:Int = 1,
-    val results:List<RemoteMovie>,
-    val totalPages:Int = 1,
-    val totalResults:Int = 0)
+    val page: Int = 1,
+    val results: List<RemoteMovie>,
+    val totalPages: Int = 1,
+    val totalResults: Int = 0
+)
